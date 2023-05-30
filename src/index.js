72668daa8ec00e75a8494dae1a409ff0f1390c87
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-05-30 19:41:48 +0300

    Set mongoose

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 const express = require("express");
-const handlebars = require("express-handlebars");
 
 const expressConfig = require("./config/expressConiguration");
 const handlebarsConfig = require("./config/handlebarsConfiguration");
+
+const dbConnect = require("./config/dbConfiguration");
 
 const routes = require("./routes");
 const app = express();
 const PORT = 5000;
 
-//Configuration
 expressConfig(app);
 handlebarsConfig(app);
+//Configuration
+dbConnect()
+  .then(() => {
+    console.log("DN connected successfully");
+  })
+  .catch((err) => {
+    console.log("DB error: ", err);
+  });
 
 app.use(routes);
 
