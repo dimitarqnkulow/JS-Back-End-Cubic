63547b9590979f068a0c8804d67017ba5fc56707
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-06-01 21:25:37 +0300

    Add get existing accessories

--- src/manager/accessoryManager.js
+++ src/manager/accessoryManager.js
@@ -1,6 +1,10 @@
 const Accessory = require("../models/Accessory");
 
-exports.getAll = () => Accessory.find();
+exports.getAll = (accessoriesId) =>
+  Accessory.find({ _id: { $nin: accessoriesId } });
+
+exports.getRestOfAccessories = (accessoriesId) =>
+  this.getAll().find({ _id: { $nin: accessoriesId } });
 
 exports.create = async (accessoryData) => {
   const newAccessory = await Accessory.create(accessoryData);
