625d697b995c707b5dc9ff5cf8cd179d67ca966a
Dimitar Yankulov <dimitarqnkulow@gmail.com>
2023-06-06 20:28:53 +0300

    Add hash password

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
   username: String,
   password: {
@@ -17,6 +17,11 @@ userSchema.virtual("repeatPassword").set(function (value) {
   if (value !== this.password) {
     throw new mongoose.MongooseError("Password missmatch!");
   }
+});
+userSchema.pre("save", async function () {
+  const hash = await bcrypt.hash(this.password, 10);
+
+  this.password = hash;
 });
 const User = mongoose.model("User", userSchema);
 
